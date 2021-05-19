f9a459457a1dbee59292acab644312384888fa3b
thuw12 <huynhminhthu12@gmail.com>
2021-05-19 20:36:38 +0700

    BillDTO them BillDetailDTO

--- src/DTO/BillDTO.java
+++ src/DTO/BillDTO.java
@@ -10,24 +10,21 @@ package DTO;
  * @author Computer
  */
 
-/*CREATE TABLE BILL(
+/*-------------TABLE BILL-------------------
+CREATE TABLE BILL(
     BILL_ID     NUMBER NOT NULL,
-    FOOD_ID     NUMBER CONSTRAINT FK_FOOD_BILL REFERENCES FOOD(FOOD_ID) NOT NULL,
-    DRINK_ID    NUMBER CONSTRAINT FK_DRINK_BILL REFERENCES DRINK(DRINK_ID) NOT NULL,
-    CUSTOMER_ID NUMBER CONSTRAINT FK_CUSTOMER_BILL REFERENCES CUSTOMER(CUSTOMER_ID) NOT NULL,
-    RESERVATION_ID  NUMBER CONSTRAINT FK_RESERVE_BILL REFERENCES RESERVATION(RESERVATION_ID) NOT NULL,
-    BILLDATE    DATE NOT NULL,
+    CUSTOMER_ID NUMBER,
+    BILLDATE    DATE,
     PAYMENTMETHOD   VARCHAR2(50),
-    TOTAL       NUMBER NOT NULL,
+    TOTAL       NUMBER,
     CONSTRAINT PK_BILL PRIMARY KEY (BILL_ID)
-)*/
+)
+*/
+
 
 public class BillDTO {
     private int BillID;
-    private int FoodID;
-    private int DrinkID;
     private int CustomerID;
-    private int ReservationID;
     private String billdate;
     private String paymentmethod;
     private int total;
@@ -35,128 +32,64 @@ public class BillDTO {
     public BillDTO() {
     }
 
-    public BillDTO(int BillID, int FoodID, int DrinkID, int CustomerID, int ReservationID, String billdate, String paymentmethod, int total) {
+    public BillDTO(int BillID, int CustomerID, String billdate, String paymentmethod) {
         this.BillID = BillID;
-        this.FoodID = FoodID;
-        this.DrinkID = DrinkID;
         this.CustomerID = CustomerID;
-        this.ReservationID = ReservationID;
+        this.billdate = billdate;
+        this.paymentmethod = paymentmethod;
+    }
+
+    public BillDTO(int BillID, int CustomerID, String billdate, String paymentmethod, int total) {
+        this.BillID = BillID;
+        this.CustomerID = CustomerID;
         this.billdate = billdate;
         this.paymentmethod = paymentmethod;
         this.total = total;
     }
 
-    /**
-     * @return the BillID
-     */
     public int getBillID() {
         return BillID;
     }
 
-    /**
-     * @param BillID the BillID to set
-     */
     public void setBillID(int BillID) {
         this.BillID = BillID;
     }
 
-    /**
-     * @return the FoodID
-     */
-    public int getFoodID() {
-        return FoodID;
-    }
-
-    /**
-     * @param FoodID the FoodID to set
-     */
-    public void setFoodID(int FoodID) {
-        this.FoodID = FoodID;
-    }
-
-    /**
-     * @return the DrinkID
-     */
-    public int getDrinkID() {
-        return DrinkID;
-    }
-
-    /**
-     * @param DrinkID the DrinkID to set
-     */
-    public void setDrinkID(int DrinkID) {
-        this.DrinkID = DrinkID;
-    }
-
-    /**
-     * @return the CustomerID
-     */
     public int getCustomerID() {
         return CustomerID;
     }
 
-    /**
-     * @param CustomerID the CustomerID to set
-     */
     public void setCustomerID(int CustomerID) {
         this.CustomerID = CustomerID;
     }
 
-    /**
-     * @return the ReservationID
-     */
-    public int getReservationID() {
-        return ReservationID;
-    }
-
-    /**
-     * @param ReservationID the ReservationID to set
-     */
-    public void setReservationID(int ReservationID) {
-        this.ReservationID = ReservationID;
-    }
-
-    /**
-     * @return the billdate
-     */
     public String getBilldate() {
         return billdate;
     }
 
-    /**
-     * @param billdate the billdate to set
-     */
     public void setBilldate(String billdate) {
         this.billdate = billdate;
     }
 
-    /**
-     * @return the paymentmethod
-     */
     public String getPaymentmethod() {
         return paymentmethod;
     }
 
-    /**
-     * @param paymentmethod the paymentmethod to set
-     */
     public void setPaymentmethod(String paymentmethod) {
         this.paymentmethod = paymentmethod;
     }
 
-    /**
-     * @return the total
-     */
     public int getTotal() {
         return total;
     }
 
-    /**
-     * @param total the total to set
-     */
     public void setTotal(int total) {
         this.total = total;
     }
+    
+    
+
+    
     
     
         
---
+++ src/DTO/BillDetailDTO.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DTO;
+
+/**
+ *
+ * @author htthi
+ */
+/*
+CREATE BILLDETAIL (
+	BILL_ID 	NUMBER,
+	ITEM_ID		NUMBER,
+	QUANTITY	NUMBER,
+	CONSTRAINT PK_CTHD PRIMARY KEY (BILL_ID, ITEM_ID)
+)
+*/
+
+public class BillDetailDTO {
+    private int BillID;
+    private int ItemID;
+    private int quantity;
+
+    public BillDetailDTO() {
+    }
+
+    public BillDetailDTO(int BillID, int ItemID, int quantity) {
+        this.BillID = BillID;
+        this.ItemID = ItemID;
+        this.quantity = quantity;
+    }
+
+    public int getBillID() {
+        return BillID;
+    }
+
+    public void setBillID(int BillID) {
+        this.BillID = BillID;
+    }
+
+    public int getItemID() {
+        return ItemID;
+    }
+
+    public void setItemID(int ItemID) {
+        this.ItemID = ItemID;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+    
+    
+}
+
+
